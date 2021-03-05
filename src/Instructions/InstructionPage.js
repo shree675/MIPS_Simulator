e3f159fa9edd06da304c8029abb87aec0b0d4dfa
shree675 <shreeteshm37@gmail.com>
2021-03-05 16:46:30 +0530

    completed instr page

--- src/Instructions/InstructionPage.js
+++ src/Instructions/InstructionPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './InstructionPage.css';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import svg1 from '../Assets/InstructionsPhase1.svg';
 
 class InfoPage extends Component {
 
@@ -12,11 +13,12 @@ class InfoPage extends Component {
         return (
             <div className="instructionpage">
                 <nav className="navbar">
-                    <div className="heading">Instructions</div>
-                    <div>
+                    <div className="heading">Instructions <span className="nutshell">in a nutshell</span></div>
+                    {/* <div> */}
                         {/* <Link className="link" to='\'><button onClick={this.return} className="return-button">HOME</button></Link> */}
-                    </div>
+                    {/* </div> */}
                 </nav>
+                <div className="svg-image"><img style={{height: `100%`, width: `100%`, objectFit: `cover`}} src={svg1}></img></div>
             </div>
         );
     }
