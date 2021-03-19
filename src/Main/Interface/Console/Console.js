97821bbe3873039e74e93e9f11f90be77455509e
shree675 <shreeteshm37@gmail.com>
2021-03-19 17:34:48 +0530

    added another button

--- src/Main/Interface/Console/Console.js
+++ src/Main/Interface/Console/Console.js
@@ -8,16 +8,29 @@ const Console = props => {
 
     function openConsole(){
         document.getElementById("console-area").style.display="block";
+        document.getElementById("pipeline-area-nf").style.display="none";
         document.getElementById("pipeline-area").style.display="none";
         document.getElementById("console-btn").style.backgroundColor="grey";
+        document.getElementById("pipeline-f").style.backgroundColor="#2e2e2e";
         document.getElementById("pipeline-nf").style.backgroundColor="#2e2e2e";
     }
 
-    function openPipeline(){
+    function openPipelinef(){
         document.getElementById("console-area").style.display="none";
         document.getElementById("pipeline-area").style.display="block";
+        document.getElementById("pipeline-area-nf").style.display="none";
+        document.getElementById("console-btn").style.backgroundColor="#2e2e2e";
+        document.getElementById("pipeline-f").style.backgroundColor="grey";
+        document.getElementById("pipeline-nf").style.backgroundColor="#2e2e2e";
+    }
+
+    function openPipelinenf(){
+        document.getElementById("console-area").style.display="none";
+        document.getElementById("pipeline-area").style.display="none";
+        document.getElementById("pipeline-area-nf").style.display="block";
         document.getElementById("console-btn").style.backgroundColor="#2e2e2e";
         document.getElementById("pipeline-nf").style.backgroundColor="grey";
+        document.getElementById("pipeline-f").style.backgroundColor="#2e2e2e";
     }
 
     var array=['CC1','CC2','CC3','CC4','CC5','CC1','CC2','CC3','CC4','CC5','CC1','CC2','CC3','CC4','CC5','CC1','CC2','CC3','CC4','CC5','CC1','CC2','CC3','CC4','CC5']
@@ -25,12 +38,15 @@ const Console = props => {
 
     function generateTable(rows,columns){
         return (
-            <table className="pipeline-table">
-                {ar2.map((eh)=>(<tr className="pip">
-                    {eh.map((e)=>(<td className="pip">{e}</td>))}
-                </tr>))}
-                
-            </table>
+            <div className="inside-pip">
+                <div>Cycles, Stalls</div>
+                <table className="pipeline-table">
+                    {ar2.map((eh)=>(<tr className="pip">
+                        {eh.map((e)=>(<td className="pip">{e}</td>))}
+                    </tr>))}
+                    
+                </table>
+            </div>
         );
     }
 
@@ -39,7 +55,8 @@ const Console = props => {
         
             <div className="console-nav">
                 <span id="console-btn" onClick={() => openConsole()}>Console</span>
-                <span id="pipeline-nf" onClick={() => openPipeline()}>Pipeline</span>
+                <span id="pipeline-f" onClick={() => openPipelinef()}>Pipeline-Forwarding</span>
+                <span id="pipeline-nf" onClick={() => openPipelinenf()}>Pipeline-NoForwarding</span>
                 {/* <span className={classPipeline} onClick={() => pipelineScreen()}>Pipeline</span> */}
             </div>
             <div id="console-area">
@@ -57,9 +74,10 @@ const Console = props => {
                 />
             </div>
             <div id="pipeline-area">
-                <div className="inside-pip">
-                    {generateTable(1,5)}
-                </div>
+                {generateTable(1,5)}
+            </div>
+            <div id="pipeline-area-nf">
+                {generateTable(1,5)}
             </div>
         </div>
     );
